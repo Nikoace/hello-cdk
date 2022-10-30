4d7457798d0f933eddd6837385a85469e58f2852
Kyo <32727925+Nikoace@users.noreply.github.com>
2022-10-30 22:27:52 +0800

    feat: s3 create

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,16 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
+import { aws_s3 as s3, RemovalPolicy } from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
+    Bucket.fromBucketArn(this, "s3Config", "")
     // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const s3Stack = new s3.Bucket(this, "myFirstBucket", {
+      versioned: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      enforceSSL: true
+    });
   }
 }
